b152070b368d578eab8bf95921669d287f0e51a0
Dylan Meivis <dylandiamonds@live.nl>
2020-03-14 14:34:51 +0100

    Create Data Model :card_file_box:

---
+++ SwiftTodo/TodoItem.swift
@@ -0,0 +1,28 @@
+//
+//  TodoItem.swift
+//  SwiftTodo
+//
+//  Created by Dylan Meivis on 14/03/2020.
+//  Copyright © 2020 Dylan Meivis. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class TodoItem: NSManagedObject, Identifiable {
+    @NSManaged public var createdAt: Data?
+    @NSManaged public var title: String?
+}
+
+extension TodoItem {
+    static func getAllTodoItems() -> NSFetchRequest<TodoItem> {
+        let request:NSFetchRequest<TodoItem> = TodoItem.fetchRequest() as!
+        NSFetchRequest<TodoItem>
+        
+        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        
+        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
